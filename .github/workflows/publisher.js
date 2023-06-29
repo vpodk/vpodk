af304ecf7803eb72e69ed09b6d9ab0c9e027a551
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2023-06-29 09:00:21 -0700

    [skip ci]

--- .github/workflows/publisher.js
+++ .github/workflows/publisher.js
@@ -7,9 +7,9 @@ const PUBLISHER_DATA = require(PUBLISHER_JSON_PATH);
 const run = () => {
   console.log(PUBLISHER_DATA);
   PUBLISHER_DATA.push(PUBLISHER_DATA.shift());
-  console.log(PUBLISHER_DATA);
 
   fs.writeFileSync(PUBLISHER_JSON_PATH, JSON.stringify(PUBLISHER_DATA), 'utf8');
+  console.log(PUBLISHER_DATA);
 };
 
 run();
