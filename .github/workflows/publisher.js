7a1c0346d61bd3aa84cb7cd8bebf52504b710ab1
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2023-06-29 10:38:35 -0700

    [skip ci]

--- .github/workflows/publisher.js
+++ .github/workflows/publisher.js
@@ -1,15 +1,52 @@
 const fs = require("fs");
 const path = require("path");
+const oauth = require("oauth");
 
 const PUBLISHER_JSON_PATH = process.argv[2];
 const PUBLISHER_JSON_DATA = require(PUBLISHER_JSON_PATH);
 
-const run = () => {
-  console.log(PUBLISHER_JSON_DATA);
-  PUBLISHER_JSON_DATA.push(PUBLISHER_JSON_DATA.shift());
+const {
+  TWITTER_API_KEY,
+  TWITTER_API_SECRET,
+  TWITTER_ACCESS_TOKEN,
+  TWITTER_ACCESS_TOKEN_SECRET,
+  INSTAGRAM_ACCESS_TOKEN,
+} = process.env;
 
+const tweet = async (/** @type {string} */ text) => {
+  const client = new oauth.OAuth(
+    "https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
+    TWITTER_API_KEY,
+    TWITTER_API_SECRET,
+    "1.0A",
+    null,
+    "HMAC-SHA1"
+  );
+
+  client.post(
+    "https://api.twitter.com/2/tweets",
+    TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_TOKEN_SECRET,
+    JSON.stringify({ text }),
+    "application/json",
+    (err, data) => {
+      if (err) {
+        console.error("[ERROR][Tweet]", err);
+      } else {
+        console.log("Tweet:", data);
+      }
+    }
+  );
+};
+
+const run = () => {
+  const post = PUBLISHER_JSON_DATA.shift();
+  PUBLISHER_JSON_DATA.push(post);
   fs.writeFileSync(PUBLISHER_JSON_PATH, JSON.stringify(PUBLISHER_JSON_DATA), 'utf8');
-  console.log(PUBLISHER_JSON_DATA);
+
+  const text = post[2] + ' ' + post[3];
+  tweet(text);
 };
 
 run();
